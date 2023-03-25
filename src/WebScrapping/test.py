c15762aad4e9213e1afd98ecb2535899d9de7d8d
Javier Santos Sanz <Javier.Santos1@alu.uclm.es>
2023-03-25 19:15:57 +0100

    Conseguido que avance entre páginas

--- src/WebScrapping/test.py
+++ src/WebScrapping/test.py
@@ -5,23 +5,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import random 
+import json
+
+# Load the JSON document from the file
+with open('../secrets.json') as f:
+    data = json.load(f)
+
 driver = webdriver.Firefox()
-driver.get("https://www.empleate.gob.es/empleo/#/trabajo?search=software&pag=0")
+driver.get("https://www.empleate.gob.es/empleo/#/trabajo?search=software&pag=" + str(data['EmpleatePage']))
 # Espera a que se cargue la página
 time.sleep(5)
 #links = driver.find_elements(By.XPATH,"//*[contains(@title,'Titulo de la oferta')]") # Selecciona los enlaces de las ofertas por su clase
 main_window = driver.current_window_handle # Guarda la pestaña principal
-
-for i in range(0,9):
-    link = driver.find_elements(By.XPATH,"//*[contains(@title,'Titulo de la oferta')]")[i] # Selecciona los enlaces de las ofertas por su clase
-    driver.execute_script("arguments[0].scrollIntoView(true);", link)
-    #si se usa link.click se buguea
-    #driver.execute_script("arguments[0].click();", link)
-    driver.execute_script("arguments[0].dispatchEvent(new MouseEvent('click', {ctrlShift: true}));", link)
-    time.sleep(3)
-    download_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "btnDescargarPdf")))
-    download_button.click()
-    driver.switch_to.window(main_window)
-    time.sleep(1)
-    #Cambiamos a la anterior ventana
-#driver.switch_to.window(main_window) # Vuelve a la pestaña principal
+while 1:
+    for i in range(0,1):
+        link = driver.find_elements(By.XPATH,"//*[contains(@title,'Titulo de la oferta')]")[i] # Selecciona los enlaces de las ofertas por su clase
+        driver.execute_script("arguments[0].scrollIntoView(true);", link)
+        #si se usa link.click se buguea, sin script
+        driver.execute_script("arguments[0].dispatchEvent(new MouseEvent('click', {ctrlShift: true}));", link)
+        
+        time.sleep(random.uniform(3.0,5.0))
+        download_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "btnDescargarPdf")))
+        download_button.click()
+        driver.switch_to.window(main_window)
+        time.sleep(random.uniform(0.5,2.0))
+    
+    driver.get("https://www.empleate.gob.es/empleo/#/trabajo?search=software&pag="+ str(data['EmpleatePage']))
+    time.sleep(random.uniform(2.0, 4.0))
+    btn_siguiente = driver.find_element(By.XPATH,"//*[contains(@title,'Siguiente')]")
+    btn_siguiente.click()
+    data['EmpleatePage'] += 1
+    with open('../secrets.json', 'w') as f:
+        json.dump(data, f)
+        print(data)
+    time.sleep(random.uniform(2.0, 5.0))
